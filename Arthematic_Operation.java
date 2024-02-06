2fb8b332a66418cbe8ca32b78524d64bc349c933
ManuhaAdlin <adlinmanuha2316@gmail.com>
2024-02-06 12:18:20 +0530

    updated

--- Arthematic_Operation.java
+++ Arthematic_Operation.java
@@ -6,8 +6,8 @@ public class Arthematic_Operation
         int x = a+b;
         int y = a-b;
         int z = a*b;
-        System.out.println("sum = " +x);
-        System.out.println("sub = " +y);
-        System.out.println("mul = " +z);
+        System.out.println("sum of 2 numbers = " +x);
+        System.out.println("differance of 2 numbers = " +y);
+        System.out.println("product of 2 numbers = " +z);
     }
 }
