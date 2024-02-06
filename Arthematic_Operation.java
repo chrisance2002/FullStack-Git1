f6188bf70c738834bc2b168ca228df4bfa55e61e
ManuhaAdlin <adlinmanuha2316@gmail.com>
2024-02-06 11:57:32 +0530

    commit

---
+++ Arthematic_Operation.java
@@ -0,0 +1,13 @@
+public class Arthematic_Operation
+{
+    public static void main(String args [])
+    {
+        int a=5, b=9;
+        int x = a+b;
+        int y = a-b;
+        int z = a*b;
+        System.out.println("sum = " +x);
+        System.out.println("sub = " +y);
+        System.out.println("mul = " +z);
+    }
+}
