529d3543a30604643cc5f420dc3c870bdc7a7e4d
ManuhaAdlin <adlinmanuha2316@gmail.com>
2024-02-06 12:08:23 +0530

    first try

---
+++ Average.java
@@ -0,0 +1,10 @@
+public class Average
+ {
+    public static void main(String args [])
+    {
+        int a=75,b=84,c=68,d=89,e=70;
+        int avg=(a+b+c+d+e)/5;
+        System.out.println("average of 5 numbers = " +avg);
+
+    }
+}
