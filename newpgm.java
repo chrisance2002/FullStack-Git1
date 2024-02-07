302f84b4190047f164c298d345fb0945ed8bb461
ManuhaAdlin <adlinmanuha2316@gmail.com>
2024-02-07 14:22:37 +0530

    first commit

---
+++ newpgm.java
@@ -0,0 +1,18 @@
+public class newpgm 
+{
+    
+        public newpgm() 
+        {
+        }
+    
+        public static void main(String[] args) 
+        {
+            newpgm p = new newpgm ();
+            System.out.println("integer=" + p.datatype(4));
+            System.out.println("char=" + p.datatype("e"));
+            System.out.println("double=" + p.datatype(3.7));
+            System.out.println("float=" + p.datatype(7.0F));
+            System.out.println("string:" + p.datatype("adlin"));
+        }
+}
+    
